9566834ba63c05c28de1aeeb18b02174df2cf0fe
cocobsccomp231p-28 <Heshan@NIBM-LAB04-PC04s-iMac.local>
2024-08-16 11:11:33 +0530

    first commit

---
+++ squaregameupdate/squaregameupdateApp.swift
@@ -0,0 +1,17 @@
+//
+//  squaregameupdateApp.swift
+//  squaregameupdate
+//
+//  Created by cocobsccomp231p-28 on 2024-08-16.
+//
+
+import SwiftUI
+
+@main
+struct squaregameupdateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
